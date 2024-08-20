fee8f1e006c462c70e6b015546ef5d9c9b54404b
Eduardo Germano <eduardor.germano@hotmail.com>
2024-08-19 22:45:56 -0300

    Ajuste da tela de login

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -4,51 +4,48 @@ import { Card } from 'primereact/card';
 import { InputText } from 'primereact/inputtext';
 import { Password } from 'primereact/password';
 import { Button } from 'primereact/button';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
     const [usuario, setUsuario] = useState({ email: "", password: "" });
     const navigate = useNavigate();
 
     const handleChange = (input) => {
-        setUsuario({ ...usuario, [input.target.name]: input.target.value })
+        setUsuario({ ...usuario, [input.target.name]: input.target.value });
     };
 
     const login = () => {
-        if (usuario.email == "eduardor.germano@hotmail.com" && usuario.password == "123") {
-            let token = "token do backend"
+        if (usuario.email === "eduardor.germano@hotmail.com" && usuario.password === "123") {
+            let token = "token do backend";
             localStorage.setItem("token", token);
             localStorage.setItem("email", usuario.email);
             navigate("/");
         } else {
-            alert("usuario ou senha incorretos");
+            alert("Usuário ou senha incorretos");
         }
-    }
+    };
 
     return (
         <div className="Body">
             <Card title="Login">
                 <div className="grid">
-                    <div className="field col-12">
-                        <label for="E-Mail">E-Mail:</label><br />
+                    <div className="email">
+                        <label htmlFor="E-Mail">E-Mail:</label>
                         <InputText onChange={handleChange} name="email" placeholder="E-Mail" />
                     </div>
-                    <div className="field col-12">
-                        <label for="password">Senha:</label><br />
-                        <Password onChange={handleChange} name="password" id="password" feedback={false} placeholder="password" toggleMask />
+                    <div className="password">
+                        <label htmlFor="password">Senha:</label>
+                        <Password onChange={handleChange} name="password" id="password" feedback={false} placeholder="Senha" toggleMask />
                     </div>
-                    <div className="col-fixed">
-                        <Button onClick={login} label="Login" />
-                    </div>
-                    <div className="col-fixed">
-                        <Button label="Recuperar Senha" />
-                    </div>
-                    <div className="col-fixed">
-                        <Button label="Cadastrar-se" />
+                    <div className="button-container">
+                        <Button onClick={login} label="Login" className="p-button" />
+                        <Button label="Recuperar Senha" className="p-button" />
+                        <Button label="Cadastrar-se" className="p-button" />
                     </div>
                 </div>
             </Card>
         </div>
     );
-}
+};
+
 export default Login;
